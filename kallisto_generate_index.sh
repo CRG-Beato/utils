fc4ef4e2b1186f6a49e08fbfbf52ebc9b0fe5810
jaquol <javier.quilez@crg.eu>
2017-12-01 13:47:33 +0100

    adapt to tRNAs, piRNAs

--- kallisto_generate_index.sh
+++ kallisto_generate_index.sh
@@ -16,16 +16,27 @@
 # Variables 
 process="kallisto_generate_index"
 species="homo_sapiens"
-version="hg38"
+version="hg38_pirnas"
 
 # Paths 
-KALLISTO=$HOME/assemblies/$species/$version/kallisto_index
 if [[ $version == "hg19" ]]; then
 	transcripts_fasta=$HOME/assemblies/$species/$version/ucsc/${version}_ensGene.fa.gz
+	KALLISTO=$HOME/assemblies/$species/$version/kallisto_index
 	index=$KALLISTO/kallisto_${species}_${version}_ensGene.index
 elif [[ $version == "hg38" ]]; then
 	transcripts_fasta=$HOME/assemblies/$species/$version/gencode/gencode.v24.transcripts.fa.gz
+	KALLISTO=$HOME/assemblies/$species/$version/kallisto_index
 	index=$KALLISTO/kallisto_${species}_${version}_gencode_v24.index
+elif [[ $version == "hg38_trnas" ]]; then
+	simple_version=`echo $version |cut -f1 -d'_'`
+	transcripts_fasta=$HOME/assemblies/$species/$simple_version/ucsc/$version.fa.gz
+	KALLISTO=$HOME/assemblies/$species/$simple_version/kallisto_index
+	index=$KALLISTO/kallisto_${species}_$version.index
+elif [[ $version == "hg38_pirnas" ]]; then
+	simple_version=`echo $version |cut -f1 -d'_'`
+	transcripts_fasta=$HOME/assemblies/$species/$simple_version/pirna_cluster_db/$version.fa.gz
+	KALLISTO=$HOME/assemblies/$species/$simple_version/kallisto_index
+	index=$KALLISTO/kallisto_${species}_$version.index
 fi
 JOB_CMD=$HOME/utils/job_cmd 
 JOB_OUT=$HOME/utils/job_out
@@ -35,7 +46,7 @@ mkdir -p $JOB_OUT
 kallisto=`which kallisto`
 
 # CRG cluster parameters
-queue=short-sl65
+queue=short-sl7
 memory=10G
 max_time=06:00:00
 slots=1
@@ -57,12 +68,13 @@ echo "#!/bin/bash
 #$ -l h_rt=$max_time
 #$ -M javier.quilez@crg.eu
 #$ -m abe
+#$ -j y
 #$ -o $m_out/${job_name}_\$JOB_ID.out
 #$ -e $m_out/${job_name}_\$JOB_ID.err
 #$ -pe smp $slots" > $job_file
 
 # build kallisto index
-job_cmd="$kallisto index -i $index $transcripts_fasta"
+job_cmd="$kallisto index -i $index $transcripts_fasta --make-unique"
 echo $job_cmd >> $job_file
 
 # Submit job
